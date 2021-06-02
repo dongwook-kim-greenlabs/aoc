fbcaa94f2fdb5fc84cfd0f55dea3cf00e48cf3b1
dongwook.kim <dw.kim@greenlabs.co.kr>
2021-06-02 13:26:41 +0900

    [ADD&REFACTOR] read-lines-as-digits function 에 docstring 추가, read-string -> java Integer.parseInt 사용하도록 변경

--- src/first_day_first_part.clj
+++ src/first_day_first_part.clj
@@ -3,7 +3,14 @@
 (require '[clojure.java.io :as io])
 
 (def file-name "firstday_input.txt")
-(defn read-lines-as-digits [file] (map read-string (clojure.string/split-lines (slurp (io/resource file)))))
+(defn read-lines-as-digits
+  "([파일명])
+    파일로부터 line들을 읽어들여 Integer 로 parsing 된 LazySeq를 반환합니다.
+
+    Example:
+    (read-lines-as-digits 'firstday_input.txt')
+    => (+19 -1 -3 +2)"
+  [file] (map #(Integer/parseInt %) (clojure.string/split-lines (slurp (io/resource file)))))
 
 (loop [acc-result 0
        data (read-lines-as-digits file-name)]
