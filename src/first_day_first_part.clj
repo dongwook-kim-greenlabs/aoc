beca87307dd8b2ca3e96f812a7717c8bbaa63e14
dongwook.kim <dw.kim@greenlabs.co.kr>
2021-06-02 14:08:53 +0900

    [REFACTOR] read-lines-as-digits 를 제거하고 utils/read-lines를 쓸수 있도록 변경

--- src/first_day_first_part.clj
+++ src/first_day_first_part.clj
@@ -1,19 +1,9 @@
 (ns first-day-first-part)
 
-(require '[clojure.java.io :as io])
-
 (def file-name "firstday_input.txt")
-(defn read-lines-as-digits
-  "([파일명])
-    파일로부터 line들을 읽어들여 Integer 로 parsing 된 LazySeq를 반환합니다.
-
-    Example:
-    (read-lines-as-digits 'firstday_input.txt')
-    => (+19 -1 -3 +2)"
-  [file] (map #(Integer/parseInt %) (clojure.string/split-lines (slurp (io/resource file)))))
 
 (loop [acc-result 0
-       data (read-lines-as-digits file-name)]
+       data (utils/read-lines file-name #(Integer/parseInt %))]
   (if (empty? data)
     acc-result
     (recur (+ acc-result (nth data 0)) (drop 1 data))))
--- src/first_day_second_part.clj
+++ src/first_day_second_part.clj
@@ -1,17 +1,14 @@
 (ns first-day-second-part)
 
-(require '[clojure.java.io :as io])
-
 (def file-name "firstday_input.txt")
-(defn read-lines-as-digits [file] (map #(Integer/parseInt %) (clojure.string/split-lines (slurp (io/resource file)))))
 (defn repeat-input [data]
   (if (= (count data) 1)
-    (read-lines-as-digits file-name)
+    (utils/read-lines file-name #(Integer/parseInt %))
     (drop 1 data)))
 
 (loop [acc-result 0
        seen #{}
-       data (read-lines-as-digits file-name)]
+       data (utils/read-lines file-name #(Integer/parseInt %))]
   (if (contains? seen acc-result)
     acc-result
     (recur (+ acc-result (first data)) (conj seen acc-result) (repeat-input data))))
