69d5d9259555cef1148c9c8f41b06d7ab357efa5
dongwook.kim <dw.kim@greenlabs.co.kr>
2021-06-02 13:27:10 +0900

    [ADD] review comment 및 개선 예제 code 추가

--- src/first_day_first_part.clj
+++ src/first_day_first_part.clj
@@ -15,5 +15,24 @@
 (loop [acc-result 0
        data (read-lines-as-digits file-name)]
   (if (empty? data)
-    (println acc-result)
+    acc-result
     (recur (+ acc-result (nth data 0)) (drop 1 data))))
+
+(map (fn [v] (Integer/parseInt v)) (clojure.string/split-lines (slurp (io/resource file-name))))
+
+(reduce + (map (fn [v] (Integer/parseInt v)) (clojure.string/split-lines (slurp (io/resource file-name)))))
+
+(->> (slurp (io/resource file-name))
+     (clojure.string/split-lines)
+     (map #(Integer/parseInt %))
+     (reduce +))
+
+;(->> input
+;     (parse)
+;     (process)
+;     (print))
+; PPAP : parse(원하는 형태로 raw input을 가공) - process - aggregate - print
+
+(comment
+  (def input (->> (slurp (io/resource file-name))
+                  (clojure.string/split-lines))))
